xiaomi-mediatek-devs/android_kernel_xiaomi_mt6877

max77932: Add missing type specifiers
Change-Id: Id5aae9d840105673f5d2bfe0bbc44dc0ef3d7ab5
Signed-off-by: bengris32 <bengris32@protonmail.ch>

drivers/power/supply/maxim/max77932.c
@@ -84,7 +84,7 @@ enum max77932_ovp {
 	MAX77932_OVP_11_0V,
 };
 
-product_name = UNKNOW;
+enum product_name product_name = UNKNOW;
 static int log_level = 2;
 
 #define max_err(fmt, ...)							\

mt6877-dev/kernel_xiaomi_pissarro

drivers/power/supply/maxim_pissarro/max77932.c
@@ -80,7 +80,7 @@ enum max77932_ovp {
 	MAX77932_OVP_11_0V,
 };
 
-product_name = PISSARRO;
+enum product_name product_name = PISSARRO;
 static int log_level = 2;
 
 #define max_err(fmt, ...)							\